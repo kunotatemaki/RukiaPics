24d2f55e2aa16d2d29413bb96d7840a4cb33e55c
Kuno Tatemaki <raulfeliz@gmail.com>
2017-07-08 16:22:10 +0200

    Added Retrofit, Gson and pojo class

--- app/src/main/java/com/rukiasoft/rukiapics/utilities/DisplayUtility.java
+++ app/src/main/java/com/rukiasoft/rukiapics/utilities/DisplayUtility.java
@@ -1,4 +1,4 @@
-package com.rukiasoft.rukiapics.ui.utilities;
+package com.rukiasoft.rukiapics.utilities;
 
 import android.app.Activity;
 import android.content.Context;
---
+++ app/src/main/java/com/rukiasoft/rukiapics/utilities/RukiaConstants.java
@@ -0,0 +1,8 @@
+package com.rukiasoft.rukiapics.utilities;
+
+/**
+ * Created by Roll on 8/7/17.
+ */
+
+public class RukiaConstants {
+}
